302804ee2af06c9f2c9d858de08f230c8d2ee150
Documenter.jl <documenter@juliadocs.github.io>
2023-05-05 13:17:50 +0000

    build based on 449aa2f

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = FFTGrp","category":"page"},{"location":"#FFTGrp","page":"Home","title":"FFTGrp","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for FFTGrp.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [FFTGrp]","category":"page"}]
+}
